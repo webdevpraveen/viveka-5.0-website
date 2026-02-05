16720e43044fbdf6a65d372ea1e3395702e4014f
Shrinjay Shresth <shrinjayshresth1@gmail.com>
2026-02-05 13:29:24 +0530

    feat: Add Organizers component to display team members categorized by role, along with a new image for a team member.

--- components/Organizers.tsx
+++ components/Organizers.tsx
@@ -64,7 +64,7 @@ const teamData = {
     
   ],
   veterans: [
-    { name: "Nandini Verma", role: "Veteran Student Convener", course: "B.Tech CSE (DS+AI), 4th Year", image: "/nandini.jpg", linkedin: "https://www.linkedin.com/in/nandini-verma-4n5v4/" },
+    { name: "Nandini Verma", role: "Veteran Student Convener", course: "B.Tech CSE (DS+AI), 4th Year", image: "/nandini-new.jpg", linkedin: "https://www.linkedin.com/in/nandini-verma-4n5v4/", objectPosition: "object-center" },
     { name: "Priyanshi Srivastava", role: "Veteran Student Convener", course: "B.Tech CSE (DS+AI), 4th Year", image: "/priyanshi.jpg", linkedin: "https://www.linkedin.com/in/priyanshi-s-012a782b1/" },
     { name: "Yash Mishra", role: "Veteran Student Convener", course: "B.Tech CSE (DS+AI), 4th Year", image: "/yash.jpg", linkedin: "https://www.linkedin.com/in/yash-mishra-2352502bb/", objectPosition: "object-bottom" },
     { name: "Kshitij Gupta", role: "Veteran Student Convener", course: "B.Tech CSE (DS+AI), 4th Year", image: "/kshitij.jpg", linkedin: "https://www.linkedin.com/in/kshitij-gupta-99a66726b/" },
